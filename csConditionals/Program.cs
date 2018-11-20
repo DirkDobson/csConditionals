171863499081ab448a01cb7a998f188531d3660b
Dirk Dobson <dirkvancouver@live.com>
2018-11-19 19:23:24 -0700

    if statment

--- csConditionals/Program.cs
+++ csConditionals/Program.cs
@@ -6,7 +6,20 @@ namespace csConditionals
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int hour = 10;
+
+            if (hour > 0&& hour < 12)
+            {
+                Console.WriteLine("It's Morning.");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("it's afternoon.");
+            }
+            else
+            {
+                Console.WriteLine("It's evening.");
+            }
         }
     }
 }
