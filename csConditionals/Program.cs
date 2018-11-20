49cbd3881f2b0b527e7a625374c72e886107b473
Dirk Dobson <dirkvancouver@live.com>
2018-11-19 19:19:58 -0700

    inial commit

---
+++ csConditionals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csConditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
